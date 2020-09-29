fd2fba953e5d47e7102ad6c1fcd5bf08b57608bc
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-09-29 16:58:11 -0300

    Atualização de dados

--- src/main/java/model/Conta.java
+++ src/main/java/model/Conta.java
@@ -1,8 +1,10 @@
 package model;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Conta {
+    @Id
     private Long id;
     private Integer agencia;
     private Integer numero;
